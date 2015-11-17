bcc8d53eee821fdd82fd9d37101f13eae8d28fb2
Claudio Cardinale <cardinale.claudio@gmail.com>
2015-11-17 17:52:07 +0100

    Authenticatesusers implemented

--- src/AuthenticatesUsers.php
+++ src/AuthenticatesUsers.php
@@ -34,6 +34,71 @@ use Illuminate\Foundation\Auth\AuthenticatesUsers  as AuthenticatesUsersOriginal
 trait AuthenticatesUsers
 {
 
-    use AuthenticatesUsersOriginal;
+    use AuthenticatesUsersOriginal
+    {
+        postLogin as postLoginOld;
+        getLogout as getLogoutOld;
+    }
+
+    /**
+     * Handle a login request to the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function postLogin(Request $request)
+    {
+        $this->validate($request, [
+            $this->loginUsername() => 'required', 'password' => 'required',
+        ]);
+
+        // If the class is using the ThrottlesLogins trait, we can automatically throttle
+        // the login attempts for this application. We'll key this by the username and
+        // the IP address of the client making these requests into this application.
+        $throttles = $this->isUsingThrottlesLoginsTrait();
+
+        if ($throttles && $this->hasTooManyLoginAttempts($request)) {
+            return $this->sendLockoutResponse($request);
+        }
+
+        $credentials = $this->getCredentials($request);
+
+        if (Auth::attempt($credentials, $request->has('remember'))) {
+            return $this->handleUserWasAuthenticated($request, $throttles);
+        }
+
+        // If the login attempt was unsuccessful we will increment the number of attempts
+        // to login and redirect the user back to the login form. Of course, when this
+        // user surpasses their maximum number of attempts they will get locked out.
+        if ($throttles) {
+            $this->incrementLoginAttempts($request);
+        }
+
+        return new JsonResponse([
+            $this->loginUsername() => $this->getFailedLoginMessage(),
+        ], 422);
+    }
+
+
+    /**
+     * Log the user out of the application.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function getLogout()
+    {
+        Auth::logout();
+    }
+
+    /**
+     * no redirect if the user performs the login
+     *
+     * @param Request $request
+     * @param Object $user
+     */
+    protected function authenticated(Request $request, $user)
+    {
+        return new JsonResponse('', 200);
+    }
 
 }
