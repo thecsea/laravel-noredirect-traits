08b8f9731a80f81f3dbeba0dc817de55f6976b9d
Claudio Cardinale <cardinale.claudio@gmail.com>
2015-11-17 01:12:24 +0100

    ResetPasswords

---
+++ src/ResetsPasswords.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Claudio Cardinale <cardi@thecsea.it>
+ * Date: 17/11/15
+ * Time: 1.06
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+namespace it\thecsea\laravel\noredirect_traits;
+
+use Illuminate\Foundation\Auth\ResetsPasswords as ResetsPasswordsTrait;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Mail\Message;
+use Illuminate\Support\Facades\Password;
+
+
+trait ResetsPasswords
+{
+    use ResetsPasswordsTrait{
+        postEmail as postEmailTrait;
+        postReset as postResetTrait;
+    }
+
+    /**
+     * Send a reset link to the given user.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function postEmail(Request $request)
+    {
+        $this->validate($request, ['email' => 'required|email']);
+
+        $response = Password::sendResetLink($request->only('email'), function (Message $message) {
+            $message->subject($this->getEmailSubject());
+        });
+
+        switch ($response) {
+            case Password::RESET_LINK_SENT:
+                return new JsonResponse('', 200);
+
+            case Password::INVALID_USER:
+                return new JsonResponse(['error' => trans($response)],422);
+            default:
+                return new JsonResponse(['error' => trans($response)],422);
+        }
+    }
+
+    /**
+     * Reset the given user's password.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function postReset(Request $request)
+    {
+        $this->validate($request, [
+            'token' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|confirmed|min:6',
+        ]);
+
+        $credentials = $request->only(
+            'email', 'password', 'password_confirmation', 'token'
+        );
+
+        $response = Password::reset($credentials, function ($user, $password) {
+            $this->resetPassword($user, $password);
+        });
+
+        switch ($response) {
+            case Password::PASSWORD_RESET:
+                return new JsonResponse('', 200);
+
+            default:
+                return new JsonResponse(['error' => trans($response)],422);
+        }
+    }
+}
